61816c387e5ce2d604b8ca3fb0f65eed3b664449
atsezgi <ezgii_ates@hotmail.com>
2020-08-12 10:42:10 +0300

    version 1 added

---
+++ src/app/movies/movies.component.ts
@@ -0,0 +1,8 @@
+import { Component, TemplateRef } from '@angular/core';
+@Component({
+    selector:'movies', //<movies></movies>
+    templateUrl: 'movies.component.html'
+})
+export class MoviesComponent{
+
+}
