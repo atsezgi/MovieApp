a0c711427e438b8fb6adf4f90818b61c2f26ab46
atsezgi <ezgii_ates@hotmail.com>
2020-08-21 08:46:34 +0300

    movie list added

---
+++ src/app/movie.datasource.ts
@@ -0,0 +1,7 @@
+import {Movie} from '../app/movie';
+
+export const Movies: Movie[] = [ //Movie from the movie.ts
+    {id:1, name : "movie 1"},
+    {id:2, name : "movie 2"},
+    {id:3, name : "movie 3"}
+];
--- src/app/movies/movies.component.ts
+++ src/app/movies/movies.component.ts
@@ -1,8 +1,19 @@
 import { Component, TemplateRef } from '@angular/core';
+import {Movies} from '../movie.datasource'
+import { Movie } from '../movie';
+
 @Component({
     selector:'movies', //<movies></movies>
-    templateUrl: 'movies.component.html'
+    templateUrl:'movies.component.html'
+
 })
 export class MoviesComponent{
+    title = "Movie List"
+    movies = Movies //Movies from the movie.datasource
+    selectedMovie: Movie;
+
+    onSelect(movie:Movie):void{
+        this.selectedMovie = movie;
+    }
 
 }
